3ddf9ca16ecd762c7e8f361e7569a3c93e00dcac
Grégory Troupenat <gregory.troupenat@gmail.com>
2020-12-05 17:16:44 +0100

    09 - Symfony : Les relations bidirectionnelles avec Doctrine

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # wild-series
+
+[Symfony-09 Screencast](https://drive.google.com/file/d/1bWoW5UuHe0Xx3Z5x5YxafDn77wncq1n4/view?usp=sharing)
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -4,6 +4,8 @@
 namespace App\Controller;
 
 use App\Entity\Program;
+use App\Entity\Season;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -33,22 +35,54 @@ class ProgramController extends AbstractController
     /**
      * Getting a program by id
      *
-     * @Route("/show/{id<^[0-9]+$>}", name="show")
+     * @Route("/{id<^[0-9]+$>}", methods={"GET"}, name="show")
+     * @param Program $program
      * @return Response
      */
-    public function show(int $id): Response
+    public function show(Program $program): Response
     {
-        $program = $this->getDoctrine()
-            ->getRepository(Program::class)
-            ->findOneBy(['id' => $id]);
-
         if (!$program) {
             throw $this->createNotFoundException(
-                'No program with id : '.$id.' found in program\'s table.'
+                'No program with id : ' . $program->getId() . ' found in program\'s table.'
             );
         }
+
+        $seasons = $program->getSeasons();
+
         return $this->render('program/show.html.twig', [
             'program' => $program,
+            'seasons' => $seasons,
+        ]);
+    }
+
+    /**
+     * @Route("/{programId<^[0-9]+$>}/seasons/{seasonId<^[0-9]+$>}", methods={"GET"}, name="season_show")
+     * @ParamConverter("program", options={"mapping": {"programId": "id"}})
+     * @ParamConverter("season", options={"mapping": {"seasonId": "id"}})
+     * @param Program $program
+     * @param Season $season
+     * @return Response
+     */
+    public function showSeason(Program $program, Season $season): Response
+    {
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : ' . $program->getId() . ' found in program\'s table.'
+            );
+        }
+
+        if (!$season) {
+            throw $this->createNotFoundException(
+                'No season with id : ' . $season->getId() . ' found in season\'s table.'
+            );
+        }
+
+        $episodes = $season->getEpisodes();
+
+        return $this->render('program/season_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
+            'episodes' => $episodes,
         ]);
     }
 }
